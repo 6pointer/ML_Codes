03464a3939d375d95996f00bbcb8bb8d55469a2d
6pointer <53047181+6pointer@users.noreply.github.com>
2019-07-19 15:43:12 +0530

    Update Tricks_pyhton.py

--- Tricks_pyhton.py
+++ Tricks_pyhton.py
@@ -1,2 +1,4 @@
 ####To see complete contents of all the cell in a df
 pd.set_option('display.max_colwidth', -1)
+####To see complete contents of all columns
+pd.set_option('display.max_columns', None)
