0ba5bebe3b7e3a2498e35730f945b9fd9e679a42
6pointer <53047181+6pointer@users.noreply.github.com>
2019-07-23 19:09:10 +0530

    Update Python_commands.py

--- Python_commands.py
+++ Python_commands.py
@@ -1,1 +1,8 @@
+### 1) MERGE COMMAND( Same as join in SQL)
+df_new = pd.merge(df1,df12,how = 'left',left_on = ['super_category','sub_category'],right_on = ['Cat','Sub_cat'])
+## Above command is a left join . Df1 and DF2 are the left and right tables and left on and right on are respective keys
 
+
+### 2) Apply Command(How to apply a function on all the rows)
+df_text['mfr_len'] =  df_text['merchant_first_reply'].apply(lambda x : len(x)) - df_text['merchant_first_reply'].apply(lambda x : x.count(' '))
+ ## Above command shows how to use apply in 2 different types of fucntions
